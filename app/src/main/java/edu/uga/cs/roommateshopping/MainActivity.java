d0b8f4c28ac52190871bb811c3a7417e568a469e
sao91172 <75396894+sao91172@users.noreply.github.com>
2023-04-17 22:48:36 -0400

    Initial commit---got register and signin to work

---
+++ app/src/main/java/edu/uga/cs/roommateshopping/MainActivity.java
@@ -0,0 +1,168 @@
+package edu.uga.cs.roommateshopping;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String DEBUG_TAG = "MainActivity";
+    private FirebaseAuth myAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        AppCompatDelegate.setDefaultNightMode( AppCompatDelegate.MODE_NIGHT_YES );
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d( DEBUG_TAG, "Roommate Shopping: MainActivity.onCreate()" );
+
+        Button signInButton = findViewById( R.id.button4 );
+        Button registerButton = findViewById(R.id.button2 );
+
+        signInButton.setOnClickListener( new SignInButtonClickListener() );
+        registerButton.setOnClickListener( new RegisterButtonClickListener() );
+    }
+
+    // A button listener class to start a Firebase sign-in process
+    private class SignInButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick( View v ) {
+            // This is an example of how to use the AuthUI activity for signing in to Firebase.
+            // Here, we are just using email/password sign in.
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build()
+            );
+
+            Log.d( DEBUG_TAG, "Roomate Shopping.SignInButtonClickListener: Signing in started" );
+
+            // Create an Intent to singin to Firebese.
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    // this sets our own theme (color scheme, sizing, etc.) for the AuthUI's appearance
+//                    .setTheme(R.style.LoginTheme)
+                    .build();
+            signInLauncher.launch(signInIntent);
+        }
+    }
+
+    // The ActivityResultLauncher class provides a new way to invoke an activity
+    // for some result.  It is a replacement for the deprecated method startActivityForResult.
+    //
+    // The signInLauncher variable is a launcher to start the AuthUI's logging in process that
+    // should return to the MainActivity when completed.  The overridden onActivityResult
+    // is then called when the Firebase logging-in process is finished.
+    private ActivityResultLauncher<Intent> signInLauncher =
+            registerForActivityResult(
+                    new FirebaseAuthUIActivityResultContract(),
+                    new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                        @Override
+                        public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                            onSignInResult(result);
+                        }
+                    }
+            );
+
+    // This method is called once the Firebase sign-in activity (lqunched above) returns (completes).
+    // Then, the current (logged-in) Firebase user can be obtained.
+    // Subsequently, there is a transition to the JobLeadManagementActivity.
+    private void onSignInResult( FirebaseAuthUIAuthenticationResult result ) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            if( response != null ) {
+                Log.d( DEBUG_TAG, "MainActivity.onSignInResult: response.getEmail(): " + response.getEmail() );
+            }
+
+            //Log.d( DEBUG_TAG, "MainActivity.onSignInResult: Signed in as: " + user.getEmail() );
+
+            // after a successful sign in, start the job leads management activity
+//            Intent intent = new Intent( this, JobLeadManagementActivity.class );
+//            startActivity( intent );
+        }
+        else {
+            Log.d( DEBUG_TAG, "MainActivity.onSignInResult: Failed to sign in" );
+            // Sign in failed. If response is null the user canceled the
+            Toast.makeText( getApplicationContext(),
+                    "Sign in failed",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class RegisterButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            // start the user registration activity
+            Intent intent = new Intent(view.getContext(), RegisterActivity.class);
+            view.getContext().startActivity(intent);
+
+        }
+    }
+
+    // These activity callback methods are not needed and are for edational purposes only
+    @Override
+    protected void onStart() {
+        Log.d( DEBUG_TAG, "Roommate Shopping: MainActivity.onStart()" );
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d( DEBUG_TAG, "Roommate Shopping: MainActivity.onResume()" );
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d( DEBUG_TAG, "Roommate Shopping: MainActivity.onPause()" );
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d( DEBUG_TAG, "Roommate Shopping: MainActivity.onStop()" );
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d( DEBUG_TAG, "JobLead: MainActivity.onDestroy()" );
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d( DEBUG_TAG, "JobLead: MainActivity.onRestart()" );
+        super.onRestart();
+    }
+}
---
+++ app/src/main/java/edu/uga/cs/roommateshopping/RegisterActivity.java
@@ -0,0 +1,136 @@
+package edu.uga.cs.roommateshopping;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    public static final String TAG = "SuperApp";
+    TextView text;
+    EditText emailEditText;
+    EditText passwordEditText;
+    Button registerButton;
+    public FirebaseAuth mAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        text = findViewById(R.id.textView3);
+        registerButton = findViewById(R.id.registerButton);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.pwEditText);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+//        String email = "dawg@mail.com";
+//        String password = "Password";
+
+        registerButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.d(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
+            }
+                });
+
+
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(getApplicationContext(),
+//                                    "Registered user: " + email,
+//                                    Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Toast.makeText(getApplicationContext(),
+//                                    "Error: " + email,
+//                                    Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+    }//bundleSavedInstance
+    public void updateUI(FirebaseUser account) {
+        if (account != null) {
+            Toast.makeText(this, "Signed in successfully", Toast.LENGTH_LONG).cancel();
+            //startActivity(new Intent(this, MainActivity.class));
+    } else {
+            Toast.makeText(this,"Please sign in", Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+
+
+        DatabaseReference myRef = database.getReference( "message" );
+       // DatabaseReference myRef = database;
+
+
+        // Read from the database value for ”message”
+        myRef.addValueEventListener( new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String message = dataSnapshot.getValue(String.class);
+                text.setText(message);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w( TAG, "Failed to read value.", error.toException() );
+            }
+        });
+
+    }
+
+}
